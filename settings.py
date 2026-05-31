22ec11c092a5f7f6af3633b57edfc28689632e19
yadavkapil23 <kydv901@gmail.com>
2026-05-31 14:57:20 +0530

    settings

---
+++ settings.py
@@ -0,0 +1,26 @@
+# settings.py
+
+# Screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# Frames per second
+FPS = 60
+
+# Colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+SKY_BLUE = (135, 206, 235)
+YELLOW = (255, 255, 0)
+BROWN = (139, 69, 19)
+
+# Physics
+GRAVITY = 0.5
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
+# Player properties
+PLAYER_JUMP = -12
