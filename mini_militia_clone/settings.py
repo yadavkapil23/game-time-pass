2794f403940d43b9d86027604f18155ee6ec39b2
yadavkapil23 <kydv901@gmail.com>
2026-05-31 21:37:46 +0530

    chore(config): add game settings configuration

---
+++ mini_militia_clone/settings.py
@@ -0,0 +1,28 @@
+# settings.py
+
+# Screen dimensions
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+
+# Colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARK_GREY = (40, 40, 40)
+LIGHT_GREY = (100, 100, 100)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+
+# Physics
+GRAVITY = 0.4
+FRICTION = -0.1
+
+# Jetpack & Player
+JETPACK_THRUST = -0.8
+PLAYER_SPEED = 5.0
+MAX_FUEL = 100.0
+FUEL_DRAIN = 1.5
+FUEL_REGEN = 1.0
